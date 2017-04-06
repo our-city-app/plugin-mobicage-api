d1c8f0800f0ea608900d54d3a40247ff031cb074
Donato <donatogomez88@gmail.com>
2017-04-06 15:14:01 +0200

    Licenses2 (#4)
    * Updated_some_more_licenses
    
    * Licenses

--- plugins/rogerthat_api/models/__init__.py
+++ plugins/rogerthat_api/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Green IT Globe NV
+# Copyright 2017 GIG Technology NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# @@license_version:1.1@@
+# @@license_version:1.3@@
--- plugins/rogerthat_api/models/settings.py
+++ plugins/rogerthat_api/models/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Green IT Globe NV
+# Copyright 2017 GIG Technology NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# @@license_version:1.1@@
+# @@license_version:1.3@@
 
 from google.appengine.ext import ndb
 
